8237089a257e6adb83d1e89533bc4705b5d9fd78
Angel Bello <abello@evopointsolution.com>
2023-03-13 09:58:57 -0400

    fix🙏 - cors config

--- DDD.Application/Program.cs
+++ DDD.Application/Program.cs
@@ -50,16 +50,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var corsConfig = "_corsConfig";
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: "",
+    options.AddPolicy(name: corsConfig,
                       policy =>
                       {
-                          policy.AllowAnyOrigin()
-                               .AllowAnyHeader()
-                                .AllowAnyMethod()
-                                .AllowCredentials();
+                          policy
+                            .AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
                       });
 });
 
@@ -77,6 +77,8 @@ app.UseSwaggerUI();
 
 
 app.UseHttpsRedirection();
+
+app.UseCors(corsConfig);
 
 app.UseAuthorization();
 
